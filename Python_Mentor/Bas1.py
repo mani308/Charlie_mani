8747311cea925df1d7eecff4ec9f23b568db2710
defaaadadafskfn8978 <manikillam308@gmail.com>
2023-02-22 16:47:51 +0530

    Without terminal

--- Python_Mentor/Bas1.py
+++ Python_Mentor/Bas1.py
@@ -2,13 +2,19 @@ import time
 import selenium
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdrivermanager import ChromeDriverManager
 
 class unknown:
     def g1(self):
         driver = webdriver.Chrome()
         driver.maximize_window()
-        driver.get("https://paytm.com/")
+        driver.get("https://www.flipkart.com/")
+        time.sleep(3)
+        driver.find_element(By.XPATH,"//button[@class='_2KpZ6l _2doB4z']").click()
+        time.sleep(3)
+        driver.find_element(By.NAME,"q").send_keys("mobiles")
+        time.sleep(3)
         driver.minimize_window()
         time.sleep(4)
         print("opened paytm")
@@ -16,8 +22,6 @@ class unknown:
 
 f1 = unknown()
 f1.g1()
-o1=unknown()
-o1.g1()
 
 
 
